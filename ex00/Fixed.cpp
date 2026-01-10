9e38eb978b589130691ebb1c8409ec3ad758afe6
quinn-sasha <quinnsashakenta@gmail.com>
2026-01-10 12:32:48 +0900

    Fixed: cpplint style

--- ex00/Fixed.cpp
+++ ex00/Fixed.cpp
@@ -1,5 +1,5 @@
-#include "Fixed.hpp"
 #include <iostream>
+#include "Fixed.hpp"
 
 const int Fixed::num_fractional_bits = 8;
 
--- ex00/Fixed.hpp
+++ ex00/Fixed.hpp
@@ -3,9 +3,9 @@
 
 class Fixed {
   int value;
-  static const int num_fractional_bits; // Always 8
+  static const int num_fractional_bits;  // Always 8
 
-public:
+ public:
   Fixed();
   Fixed(const Fixed& rhs);
   Fixed& operator=(const Fixed& rhs);
@@ -15,4 +15,4 @@ public:
   void setRawBits(int const raw);
 };
 
-#endif // EX00_FIXED_HPP_
+#endif  // EX00_FIXED_HPP_
